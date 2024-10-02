ef7afff47d22fc030d7a6012b46a3df71db812f4
Kónya Benedek <konya.benedek@burg.lo.hu>
2024-10-02 10:10:52 +0200

    Teszt adatok beolvasás adatbázisból és azok megjelenítése

--- ProjKozmetika/Dolgozo.cs
+++ ProjKozmetika/Dolgozo.cs
@@ -8,20 +8,31 @@ namespace ProjKozmetika
 {
     public class Dolgozo
     {
+        byte id;
         string dolgFirstName, dolgLastName, dolgTel, dolgEmail;
         bool status;
-        int szolgaltatas;
+        byte szolgaltatas;
 
-        public Dolgozo()
+        public Dolgozo(byte id, string dolgFirstName, string dolgLastName, string dolgTel, string dolgEmail, bool status, byte szolgaltatas)
         {
-
+            
+            Id = id;
+            DolgFirstName = dolgFirstName;
+            DolgLastName = dolgLastName;
+            DolgTel = dolgTel;
+            DolgEmail = dolgEmail;
+            Status = status;
+            Szolgaltatas = szolgaltatas;
         }
 
+        public byte Id { get => id; set => id = value; }
         public string DolgFirstName { get => dolgFirstName; set => dolgFirstName = value; }
         public string DolgLastName { get => dolgLastName; set => dolgLastName = value; }
         public string DolgTel { get => dolgTel; set => dolgTel = value; }
         public string DolgEmail { get => dolgEmail; set => dolgEmail = value; }
         public bool Status { get => status; set => status = value; }
-        public int Szolgaltatas { get => szolgaltatas; set => szolgaltatas = value; }
+        public byte Szolgaltatas { get => szolgaltatas; set => szolgaltatas = value; }
+
+        public string DolgozoFullName => $"{DolgLastName} {DolgFirstName}";
     }
 }
--- ProjKozmetika/MainWindow.xaml.cs
+++ ProjKozmetika/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MySqlConnector;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
 using System.Text;
@@ -21,16 +22,23 @@ namespace ProjKozmetika
     {
         MySqlConnection conn;
         private readonly string connectionString = "server=localhost;port=3306;uid=root;database=kozmetika";
+        ObservableCollection<Szolgaltatas> szolgatatasok = new ObservableCollection<Szolgaltatas>();
+        ObservableCollection<Dolgozo> dolgozok = new ObservableCollection<Dolgozo>();
         
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_LoadedAsync;
+            ServiceComboBox.ItemsSource = szolgatatasok;
+            EmployeeComboBox.ItemsSource = dolgozok;
+            this.DataContext = this;
         }
-        
+
         private async void MainWindow_LoadedAsync(object sender, RoutedEventArgs e)
         {
-            await ConnectionAsync();
+            //await ConnectionAsync();
+            await GetServicesAsync();
+            await GetWorkersAsync();
         }
 
         private async Task<Task> ConnectionAsync()
@@ -44,11 +52,83 @@ namespace ProjKozmetika
             {
                 MessageBox.Show(ex.Message, "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            if (conn.State == ConnectionState.Open)
-            {
-                MessageBox.Show($"Az alkalmazás sikeresen csatlakozott a(z) {conn.Database} nevű adatbázishoz.", "Sikeres csatlakozás!", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
             return Task.CompletedTask;
+        }
+        private async Task GetServicesAsync()
+        {
+            try
+            {
+                conn = new MySqlConnection(connectionString);
+                await conn.OpenAsync();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            string query = "SELECT szolgaltatasID, szolgaltatasKategoria, szolgaltatasIdotartam, szolgaltatasAr FROM Szolgáltatás";
+
+            var command = new MySqlCommand(query, conn);
+            var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                szolgatatasok.Add(new Szolgaltatas(reader.GetByte(0), reader.GetString(1), reader.GetTimeSpan(2), reader.GetInt32(3)));
+            }
+            await conn.CloseAsync();
+        }
+
+        private async Task GetWorkersAsync()
+        {
+            try
+            {
+                conn = new MySqlConnection(connectionString);
+                await conn.OpenAsync();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            string query = "SELECT dolgozoID, dolgozoFirstName, dolgozoLastName, dolgozoTel, dolgozoEmail, statusz, szolgáltatasa FROM dolgozók";
+
+            var command = new MySqlCommand(query, conn);
+            var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                if (reader.GetBoolean(5) == false)
+                {
+                    continue;
+                }
+                else dolgozok.Add(new Dolgozo(reader.GetByte(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetBoolean(5), reader.GetByte(6)));
+            }
+            await conn.CloseAsync();
+        }
+
+        private async Task IsCorrectWorkerAsync()
+        {
+            var chosenWorker = EmployeeComboBox.SelectedItem.ToString().Split(" ");
+            var chosenService = ServiceComboBox.SelectedItem.ToString();
+            var worker = dolgozok.ToList().Find(x => x.DolgLastName == chosenWorker[0] && x.DolgFirstName == chosenWorker[1]);
+            var service = szolgatatasok.ToList().Find(x => x.SzolgKategoria == chosenService);
+
+            if (worker.Szolgaltatas != service.SzolgID)
+            {
+                MessageBox.Show("Figyelem! Az általad válaszott dolgozó nem biztosítja a válaszott szolgáltatás!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            EmployeeComboBox.SelectedItem = null;
+            ServiceComboBox.SelectedItem = null;
+        }
+
+        private async void EmployeeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           await IsCorrectWorkerAsync();
+        }
+
+        private async void ServiceComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await IsCorrectWorkerAsync();
         }
     }
 }
